4593e792e9ed3604564b782fcd85e490df56c5d3
Amos Baya <amosbaya96@gmail.com>
2025-07-31 10:38:50 +0300

    feat(backend): removed authentication from product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
-const { authenticate } = require('../middlewares/authMiddleware');
-
-
-router.use(authenticate);
 
 //creating a product
 router.post('/create', productController.createProduct);
